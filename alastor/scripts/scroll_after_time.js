ef2bfa9df5514136e1c4275ca8b8787ea8ab3b2a
Krzysztof Matuszewski <krzysiekmatuszewski@outlook.com>
2021-03-16 16:27:58 +0100

    Delete alastor directory

--- alastor/scripts/scroll_after_time.js
+++
@@ -1,9 +0,0 @@
-
-let after = 5000; // time in ms
-
-$(function () {
-    setTimeout(function () { goToSecondTab(); }, after);
-    function goToSecondTab() {
-        window.location.hash = '#navbar';
-    }
-});
